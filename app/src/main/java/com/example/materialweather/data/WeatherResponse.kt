1fba063ab8e4377a2be81266ea6676b6c42218d0
parneet-guraya <gurayaparneet@gmail.com>
2024-04-04 05:12:04 +0530

    use correct convertor annot
    Signed-off-by: parneet-guraya <gurayaparneet@gmail.com>

--- app/src/main/java/com/example/materialweather/data/WeatherResponse.kt
+++ app/src/main/java/com/example/materialweather/data/WeatherResponse.kt
@@ -5,49 +5,49 @@ import com.squareup.moshi.JsonClass
 
 @JsonClass(generateAdapter = true)
 data class WeatherResponse(
-    @field:Json(name = "location") val location: Location?,
-    @field:Json(name = "current") val current: Current?
+    @Json(name = "location") val location: Location?,
+    @Json(name = "current") val current: Current?
 )
 
 data class Location(
-    @field:Json(name = "name") val name: String?,
-    @field:Json(name = "region") val region: String?,
-    @field:Json(name = "country") val country: String?,
-    @field:Json(name = "lat") val lat: Double?,
-    @field:Json(name = "lon") val lon: Double?,
-    @field:Json(name = "tz_id") val tzId: String?,
-    @field:Json(name = "localtime_epoch") val localtimeEpoch: Long?,
-    @field:Json(name = "localtime") val localtime: String?
+    @Json(name = "name") val name: String?,
+    @Json(name = "region") val region: String?,
+    @Json(name = "country") val country: String?,
+    @Json(name = "lat") val lat: Double?,
+    @Json(name = "lon") val lon: Double?,
+    @Json(name = "tz_id") val tzId: String?,
+    @Json(name = "localtime_epoch") val localtimeEpoch: Long?,
+    @Json(name = "localtime") val localtime: String?
 )
 
 data class Current(
-    @field:Json(name = "last_updated_epoch") val lastUpdatedEpoch: Long?,
-    @field:Json(name = "last_updated") val lastUpdated: String?,
-    @field:Json(name = "temp_c") val tempC: Double?,
-    @field:Json(name = "temp_f") val tempF: Double?,
-    @field:Json(name = "is_day") val isDay: Int?,
-    @field:Json(name = "condition") val condition: Condition?,
-    @field:Json(name = "wind_mph") val windMph: Double?,
-    @field:Json(name = "wind_kph") val windKph: Double?,
-    @field:Json(name = "wind_degree") val windDegree: Int?,
-    @field:Json(name = "wind_dir") val windDir: String?,
-    @field:Json(name = "pressure_mb") val pressureMb: Double?,
-    @field:Json(name = "pressure_in") val pressureIn: Double?,
-    @field:Json(name = "precip_mm") val precipMm: Double?,
-    @field:Json(name = "precip_in") val precipIn: Double?,
-    @field:Json(name = "humidity") val humidity: Int?,
-    @field:Json(name = "cloud") val cloud: Int?,
-    @field:Json(name = "feelslike_c") val feelslikeC: Double?,
-    @field:Json(name = "feelslike_f") val feelslikeF: Double?,
-    @field:Json(name = "vis_km") val visKm: Double?,
-    @field:Json(name = "vis_miles") val visMiles: Double?,
-    @field:Json(name = "uv") val uv: Double?,
-    @field:Json(name = "gust_mph") val gustMph: Double?,
-    @field:Json(name = "gust_kph") val gustKph: Double?
+    @Json(name = "last_updated_epoch") val lastUpdatedEpoch: Long?,
+    @Json(name = "last_updated") val lastUpdated: String?,
+    @Json(name = "temp_c") val tempC: Double?,
+    @Json(name = "temp_f") val tempF: Double?,
+    @Json(name = "is_day") val isDay: Int?,
+    @Json(name = "condition") val condition: Condition?,
+    @Json(name = "wind_mph") val windMph: Double?,
+    @Json(name = "wind_kph") val windKph: Double?,
+    @Json(name = "wind_degree") val windDegree: Int?,
+    @Json(name = "wind_dir") val windDir: String?,
+    @Json(name = "pressure_mb") val pressureMb: Double?,
+    @Json(name = "pressure_in") val pressureIn: Double?,
+    @Json(name = "precip_mm") val precipMm: Double?,
+    @Json(name = "precip_in") val precipIn: Double?,
+    @Json(name = "humidity") val humidity: Int?,
+    @Json(name = "cloud") val cloud: Int?,
+    @Json(name = "feelslike_c") val feelslikeC: Double?,
+    @Json(name = "feelslike_f") val feelslikeF: Double?,
+    @Json(name = "vis_km") val visKm: Double?,
+    @Json(name = "vis_miles") val visMiles: Double?,
+    @Json(name = "uv") val uv: Double?,
+    @Json(name = "gust_mph") val gustMph: Double?,
+    @Json(name = "gust_kph") val gustKph: Double?
 )
 
 data class Condition(
-    @field:Json(name = "text") val text: String?,
-    @field:Json(name = "icon") val icon: String?,
-    @field:Json(name = "code") val code: Int?
+    @Json(name = "text") val text: String?,
+    @Json(name = "icon") val icon: String?,
+    @Json(name = "code") val code: Int?
 )
